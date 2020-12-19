67fdf568e50a13939d1f07f119fbdca34f9c1c60
System Administrator <root@MBA1.local>
2020-12-19 12:06:32 +0700

    add coloumn in porodusen

---
+++ database/migrations/2020_12_19_044306_add_coloumn_in_table_produsen.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColoumnInTableProdusen extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tj_produsen', function($table) {
+            $table->integer('village_id')->unsigned();
+            $table->integer('district_id')->unsigned();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tj_produsen', function($table) {
+            $table->dropColumn('village_id');
+            $table->dropColumn('district_id');
+        });
+    }
+}
