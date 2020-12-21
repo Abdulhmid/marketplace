4f1d83c888ad6f17d124337be649b92a058f6fe2
System Administrator <root@MBA1.local>
2020-12-22 00:15:18 +0700

    setup buy

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(RolesSeeder::class);
-        $this->call(InputWilayahIndo::class);
+        // $this->call(InputWilayahIndo::class);
         // $this->call(MenusSeeders::class);
         // Users Seeder
         $data = [
