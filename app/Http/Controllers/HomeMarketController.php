76cc3ba2c0b4c93ff17b43fc9fbd8e87e90226eb
abdulhamid <abdulhamid909@gmail.com>
2021-05-19 12:47:22 +0700

    rev query

--- app/Http/Controllers/HomeMarketController.php
+++ app/Http/Controllers/HomeMarketController.php
@@ -66,8 +66,8 @@ class HomeMarketController extends Controller
         $banners = $banners->latest()->get();
     	$products = $products
                         ->with(['category','variant'])
-                        ->whereHas('seller') // hanya produk yg sudah dipilih seller yg bisa dijual
-                        ->where('status',1)
+                        // ->whereHas('seller') // hanya produk yg sudah dipilih seller yg bisa dijual
+                        // ->where('status',1)
     					->latest();
     	$product_category = $product_category->latest()->get();
         $menus = $menus->orderBy('sort','asc')->get();
